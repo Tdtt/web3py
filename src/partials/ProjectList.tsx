0c7b0672d080e51ef94b5354164433350993137e
Tdtt <86044842+Tdtt@users.noreply.github.com>
2022-09-08 04:41:58 +0800

    Update ProjectList.tsx

--- src/partials/ProjectList.tsx
+++ src/partials/ProjectList.tsx
@@ -33,32 +33,31 @@ const ProjectList = () => (
         }
       />
       <Project
-        name="Project 2"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-        bibendum. Nunc non posuere consectetur, justo erat semper enim, non
-        hendrerit dui odio id enim."
+        name="合约交易"
+        description="华尔街没有新鲜事,投机如山岳般古老. 用自己真金白银总结出来的经验加上自己的交易习惯才是适合自己的最好交易策略"
         link="/"
         img={{ src: '/assets/images/project-fire.png', alt: 'Project Fire' }}
         category={
           <>
-            <Tags color={ColorTags.VIOLET}>Next.js</Tags>
-            <Tags color={ColorTags.EMERALD}>Blog</Tags>
-            <Tags color={ColorTags.YELLOW}>JavaScript</Tags>
+            <Tags color={ColorTags.VIOLET}>TradingView</Tags>
+            <Tags color={ColorTags.EMERALD}>Fibonacci</Tags>
+            <Tags color={ColorTags.YELLOW}>支撑阻力</Tags>
+             <Tags color={ColorTags.SKY}>成交量分布</Tags>
           </>
         }
       />
       <Project
-        name="Project 3"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-        bibendum. Nunc non posuere consectetur, justo erat semper enim, non
-        hendrerit dui odio id enim."
+        name="VC"
+        description="去年投资了很多项目,经历了牛市的疯狂和熊市的残酷.从牛市的哪怕项目方是头猪都能跟着风口起飞,
+                     到熊市的优质项目开盘破发.果然要经历牛熊才能认清自己.理智投资"
         link="/"
         img={{ src: '/assets/images/project-maps.png', alt: 'Project Maps' }}
         category={
           <>
-            <Tags color={ColorTags.FUCHSIA}>Astro.js</Tags>
-            <Tags color={ColorTags.INDIGO}>Bootstrap</Tags>
-            <Tags color={ColorTags.ROSE}>TypeScript</Tags>
+            <Tags color={ColorTags.FUCHSIA}>项目背景</Tags>
+            <Tags color={ColorTags.INDIGO}>合作伙伴</Tags>
+            <Tags color={ColorTags.ROSE}>IDO平台</Tags>
+            <Tags color={ColorTags.FUCHSIA}>市场趋势</Tags>
           </>
         }
       />
